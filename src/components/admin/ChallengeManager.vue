5a08654e15c9be05a7201d9630b741e0fc2e0e69
paranyo <gksrbxo123@gmail.com>
2019-08-03 23:41:09 +0900

    REPAIR: Tag View (ChallengeManager) 작업중
    Refresh 하면 렌더링 안되는 오류 발견. 해결 해야함.
    Add Tag 후 바로 렌더링되게끔 해야함.
    태그 클릭시 해당 태그에 연결되어있는 문제 출력해야함.

--- src/components/admin/ChallengeManager.vue
+++ src/components/admin/ChallengeManager.vue
@@ -1,18 +1,16 @@
 <template>
 	<div class="container-fluid">
     <div class="container-align">
-			<div class="btn-group btn-group-lg btn-group-toggle" data-toggle="buttons" role="group">
 			<!--
 				<label class="btn btn-secondary active" v-for="tag in tags" :key="tag.title" :title="tag.title">
 					<input type="checkbox" :title="tag.title" autocomplete>
 					<router-link class="btn-sizing" :to="`/manage/challenge/${tag.title}`">{{ tag.title }}</router-link>
 				</label>
 				-->
-				<b-form-group>
-					<b-form-checkbox-group :options="options" class="btn-group-lg" buttons></b-form-checkbox-group>
-				</b-form-group>
-				<button type="button" class="btn btn-secondary" @click="createTag">&plus;</button>
-    	</div>
+			<b-form-group>
+				<b-form-checkbox-group :options="options" class="btn-group-lg" buttons @click="show"></b-form-checkbox-group>
+				<button type="button" class="btn btn-danger btn-lg" @click="createTag">&plus;</button>
+			</b-form-group>
 		</div>
 		<AddCategory v-if="isAddTag" />
 		<router-view />
@@ -25,9 +23,7 @@ import ProbField	 from './ProbField.vue'
 export default {
 	data() {
 		return {
-			options: [
-				
-			]
+			options: []
 		}
 	},
 	components: { AddCategory, ProbField },
@@ -54,14 +50,13 @@ export default {
 		fetchOptions() {
 			this.tags.map(t => this.options.push({ text: t.title }))
 		},
+		show() {
+	
+		}
 	}
 }
 </script>
 <style scope>
-.tags {
-	display: inline-block;
-	border: 1px solid #ccc;
-}	
 
 .container-align {
 	width: 100%;
