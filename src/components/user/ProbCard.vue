1ef1f31574fae3c0511e783edf409cc8e881c242
paranyo <gksrbxo123@gmail.com>
2020-01-31 23:58:20 +0900

    ProbCard 디자인
    개구진 디자인 감각을 한계로 끌어 올려 보다 현대적인(?)
    카드로 디자인하였다.

--- src/components/user/ProbCard.vue
+++ src/components/user/ProbCard.vue
@@ -1,4 +1,27 @@
 <template>
+	<b-card no-body class="overflow-hidden" style="max-width: 540px; height: 210px;" @click="showProb" :class="{'border-danger': prob.deletedAt }">
+  	<b-row no-gutters style="height: 100%;">
+    	<b-col md="3" style="height: 100%;">
+        <p v-if="prob.isCorrect" style="font-size: 32pt; transform: rotate(300deg); margin: 60px 0 0 -10px; font-weight: 400; position: absolute; text-align: center; color: #77DD77;">Correct!</p>
+        <p v-else style="font-size: 36pt; transform: rotate(300deg); margin: 60px 0 0 10px; font-weight: 100; position: absolute; text-align: center;" :style="{'color': fontColor }">{{ prob.tag }}</p>
+        <p style="position: absolute; width: 100%; text-align: center; margin: 10px 0; bottom: 0; color: #ccc; font-weight: lighter;"><b-badge variant="primary">paranyo</b-badge></p>
+      </b-col>
+      <b-col md="9">
+        <b-card-body style="height: 100%;">
+          <b-card-title style="margin: 0; height: 20%; font-size: 14pt;" class="overText">
+						<h5 style="display: inline;"><b-badge variant="secondary">{{ prob.score }}</b-badge></h5>&nbsp;{{ prob.title }}
+          </b-card-title>
+          <b-card-text style="margin: 0; height: 60%; padding-top: 5px;">
+						{{ description }}
+          </b-card-text>
+          <b-card-text style="margin: 0; height:20%; padding-top: 10px; text-align: right;">
+            <p>{{ prob.solver }}명이 풀었습니다.</p>
+        	</b-card-text>
+      	</b-card-body>
+    	</b-col>
+  	</b-row>
+	</b-card>
+<!--
 	<div class="card bg-light mb-3 h-100" @click="showProb">
 	  <div class="card-header px-2 py-2" :class="{'bg-danger': prob.deletedAt}">
 			<span class="badge badge-pill badge-secondary">{{ prob.tag }}</span>
@@ -13,25 +36,34 @@
 	  <div class="card-footer" :class="{'bg-danger': prob.deletedAt}">
 			<p class="card-text"><small class="text-muted">{{ prob.solver }}명이 풀었습니다.</small></p>
 		</div>
-  </div>
+  </div>-->
 </template>
 <script>
 import { mapState, mapActions, mapMutations } from 'vuex'
 export default {
 	props: ['prob'],
 	computed: {
-		substrTitle() {
-			if(this.prob.title.length > 10)
-				return this.prob.title.substring(0, 10) + '...'
-			else
-				return this.prob.title
-		},
-		substrDesc() {
-			if(this.prob.description.length > 32)
-				return this.prob.description.substring(0, 32) + '...'
+		description() {
+			if(this.prob.description.length > 81)
+				return " " + this.prob.description.substr(0, 82) + "..."
 			else
 				return this.prob.description
-		}
+		},
+		fontColor() {
+			let t = this.prob.tag
+			if(t == 'WEB')
+				return '#FDDFDF'
+			else if(t == 'FOR')
+				return '#90C978'
+			else if(t == 'REV')
+				return '#83C6DD'
+			else if(t == 'PWN')
+				return '#5DB1D1'
+			else if(t == 'MISC')
+				return '#FF756D'
+			else
+				return '#000000'
+		},
 	},
 	methods: {
 		...mapActions(['FETCH_PROBS']),
@@ -44,4 +76,10 @@ export default {
 }
 </script>
 <style scoped>
+.overText {
+	width: 100%;
+	white-space: nowrap;
+	overflow: hidden;
+	text-overflow: ellipsis;
+}
 </style>
